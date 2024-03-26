427e3f73152670227f36c754f27ff26773aa159f
Victor <vikad111@gmail.com>
2024-03-26 01:22:02 +0100

    windows distr

--- etc/build-win.bat
+++ etc/build-win.bat
@@ -1,15 +1,16 @@
 setlocal
 
 chcp 65001
-cd %~dp0..\..
+cd %~dp0..
 
 set JAVA_HOME=%JAVA17_HOME%
 
-rem start mvn clean package -DskipTests
+rem call mvn clean package -DskipTests
 
 "%JAVA_HOME%\bin\jpackage.exe" ^
     --type exe ^
-    --input target\distr\lib ^
+    --input target\out\lib ^
+    --dest target\distr\win ^
     --main-jar zooinspector-n-1.1.RC1.jar ^
     --main-class org.apache.zookeeper.inspector.ZooInspector
     
