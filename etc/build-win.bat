70a9a95f858f5f5f4417bff1d052bf92160a6b19
Victor <vikad111@gmail.com>
2024-03-29 12:55:51 +0100

    win distr

--- etc/build-win.bat
+++ etc/build-win.bat
@@ -8,7 +8,15 @@ set JAVA_HOME=%JAVA17_HOME%
 call mvn clean package -DskipTests
 
 "%JAVA_HOME%\bin\jpackage.exe" @target\jpackage-win.args --type app-image
-"%JAVA_HOME%\bin\jpackage.exe" @target\jpackage-win.args --type exe
-    
- 
+"%JAVA_HOME%\bin\jpackage.exe" @target\jpackage-win.args ^
+	--type exe ^
+	--win-per-user-install ^
+	--win-shortcut ^
+	--win-menu ^
+	--win-menu-group ZooInspector ^
+	--win-upgrade-uuid a68e451c-3cae-4e4a-bf2b-eedf7c501414
+
+rem	--win-dir-chooser ^
+rem	--win-shortcut-prompt ^
+
 endlocal
